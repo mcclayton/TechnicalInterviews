8ede0e54f541d7f344bf438334b15be499bd423b
Michael Clayton <mostly_mike@hotmail.com>
2017-02-21 19:28:09 -0800

    Update comment

--- Arrays/MaximumSumSubarray.java
+++ Arrays/MaximumSumSubarray.java
@@ -19,17 +19,6 @@ public class MaximumSumSubarray {
 
     /**
      * Kadane's Algorithm:
-     * Initialize:
-     * max_so_far = 0
-     * max_ending_here = 0
-     *
-     * Loop for each element of the array
-     * (a) max_ending_here = max_ending_here + a[i]
-     * (b) if(max_ending_here < 0)
-     *        max_ending_here = 0
-     * (c) if(max_so_far < max_ending_here)
-     *        max_so_far = max_ending_here
-     * return max_so_far
      */
     private static int maximumSubarraySum(int[] arr) {
         int globalMax = arr[0];
