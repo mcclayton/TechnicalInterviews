cedfcdfc0e2cc9dea62cf97daf5136a32d171ce7
Michael Clayton <mostly_mike@hotmail.com>
2017-02-22 22:10:41 -0800

    Fixed bug in MaximumSumSubarray problem.

--- Arrays/MaximumSumSubarray.java
+++ Arrays/MaximumSumSubarray.java
@@ -15,6 +15,9 @@ public class MaximumSumSubarray {
     public static void main (String[] args) throws java.lang.Exception {
         System.out.println(maximumSubarraySum(new int[] { -2, -3, 4, -1, -2, 1, 5, -3 }));    // 7
         System.out.println(maximumSubarraySum(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));  // 6
+        System.out.println(maximumSubarraySum(new int[] { 3, 5, 0, -5 }));                    // 5
+        System.out.println(maximumSubarraySum(new int[] { 10 }));                             // 10
+        System.out.println(maximumSubarraySum(new int[] { -10 }));                            // -10
     }
 
     /**
@@ -24,7 +27,9 @@ public class MaximumSumSubarray {
         int globalMax = arr[0];
         int maxCurrent = arr[0];
 
-        for (int num : arr) {
+        int num;
+        for (int i=1; i<arr.length; i++) {
+            num = arr[i];
             maxCurrent = Math.max(maxCurrent + num, num);
             globalMax = Math.max(globalMax, maxCurrent);
         }
