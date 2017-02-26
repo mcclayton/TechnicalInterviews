a3fe389bd0b5524d1c3dd407121eeae93cf7bd72
Michael Clayton <mostly_mike@hotmail.com>
2017-02-26 13:22:10 -0800

    Update comment in StrobogrammaticNumber problem

--- Strings/StrobogrammaticNumber.java
+++ Strings/StrobogrammaticNumber.java
@@ -8,7 +8,7 @@ import java.math.*;
  * 180 degrees (looked at upside down).
  * Write a function to determine if a number is strobogrammatic.
  * The number is represented as a string.
- * For example, the numbers "69", "88", and "818" are all strobogrammatic..
+ * For example, the numbers "96", "808", and "818" are all strobogrammatic.
  */
 
 public class StrobogrammaticNumber {
